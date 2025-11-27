70217fdebe75c104c4c922d9941c3435c682c9a7
Sid <thesenjubusiness@gmail.com>
2025-11-28 00:28:00 +0530

    Portfolio gng

---
+++ portfolio.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".portfolio-container");
+
+  fetch("./portfolio.json")
+    .then(res => {
+      if (!res.ok) throw new Error("JSON not found");
+      return res.json();
+    })
+    .then(data => {
+      data.forEach(item => {
+        const card = document.createElement("div");
+        card.className = "portfolio-card";
+
+        card.innerHTML = `
+          <a href="${item.url}" target="_blank" class="card-link">
+          <div class="portfolio-title">${item.title}</div>
+          <div class="portfolio-desc">${item.description}</div>
+          <div class="tech-stack">
+            ${item.tech.map(t => `<span class="tech">${t}</span>`).join("")}
+          </div>
+          </a>
+        `;
+
+        container.appendChild(card);
+      });
+    })
+    .catch(err => {
+      container.innerHTML = "<p style='color:red'>Portfolio failed to load</p>";
+      console.error(err);
+    });
+});
